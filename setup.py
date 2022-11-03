49cf42bb3a68944c718f4f0f5ff23a3afc8335b5
Matheus Viana <vianamp@gmail.com>
2022-11-03 13:08:51 -0700

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -84,6 +84,6 @@ setup(
     url="https://github.com/AllenCell/aics-shparam",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
