c80c7d0291e5fbf35043a0f1199082dbfdbb0d38
Matheus Viana <vianamp@gmail.com>
2023-04-17 19:53:36 -0700

    Bump version: 0.1.5 → 0.1.6

--- aicsshparam/__init__.py
+++ aicsshparam/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Matheus Viana"
 __email__ = "matheus.viana@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 
 def get_module_version():
