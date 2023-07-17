de53f3554e8218e80da3179524fbd1ab771770fe
jessicasyu <15913767+jessicasyu@users.noreply.github.com>
2023-07-17 14:55:44 -0400

    Switch docs theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,6 @@
 import os
 import sys
 
-import sphinx_rtd_theme
-
 import aicsshparam
 
 sys.path.insert(0, os.path.abspath(".."))
@@ -106,16 +104,11 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
-#
-html_theme_options = {
-    "collapse_navigation": False,
-    "prev_next_buttons_location": "top",
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
