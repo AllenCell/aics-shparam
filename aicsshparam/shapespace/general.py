9093d120d2a49ddaf5c5b75bf1faf5a6bb624849
Philip Garrison <philip.garrison@alleninstitute.org>
2024-03-19 10:55:29 -0700

    Remove json-reading code

--- aicsshparam/shapespace/general.py
+++
@@ -1,8 +0,0 @@
-import yaml
-from pathlib import Path
-
-
-def load_config_file(path: Path, fname="config.yaml"):
-    with open(Path(path)/fname, "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    return config
